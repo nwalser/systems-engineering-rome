5f3534a9d926b61e32e7986c9f8e4f3bb778754e
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-10-21 10:42:24 +0200

    added rome

---
+++ praktikum3/solutions.py
@@ -0,0 +1,11 @@
+﻿import spatialmath as sm
+import roboticstoolbox as rtb
+import os
+
+
+os.getcwd()
+os.chdir("C:/Users/NathanielWalser/Documents/Repositories/systems-engineering-rome/praktikum3")
+
+from IRB1100 import IRB1100
+irb1100 = IRB1100()
+print(irb1100)
