6bdd937d6d3e1cfdef90b26ed5719f9557cb69c5
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-10-31 13:18:34 +0100

    added lab4 initial documents

---
+++ praktikum4/OmniCore.py
@@ -0,0 +1,37 @@
+class OmniCore:
+    
+    """control an ABB robot with Python"""
+
+    def __init__(self, ipAddress):
+
+        import socket
+        PORT = 8400  # the port used by the server
+        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((ipAddress, PORT))
+        self.s.settimeout(60.0)
+
+    def __del__(self):
+
+        import socket
+        
+        b = bytes("Close\r\n", 'utf-8')
+        self.s.sendall(b)
+        self.s.shutdown(socket.SHUT_RDWR)
+        self.s.close()
+
+    def home(self):
+        
+        b = bytes("Home\r\n", 'utf-8')
+        self.s.sendall(b)
+        data = self.s.recv(1024)
+        
+        return data
+
+    def moveAbsJ(self, theta):
+        
+        b = bytes(f"MoveAbsJ {theta[0]:.3f} {theta[1]:.3f} {theta[2]:.3f} {theta[3]:.3f} {theta[4]:.3f} {theta[5]:.3f}\r\n", 'utf-8')
+        self.s.sendall(b)
+        data = self.s.recv(1024)
+        
+        return data
